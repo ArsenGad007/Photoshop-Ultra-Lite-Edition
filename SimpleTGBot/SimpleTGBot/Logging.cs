61454396d163957fd640c93b8b5692eee8c41db3
Arsen Gadzhiyev <arsen.gadzhiyev.00@mail.ru>
2025-04-15 20:01:48 +0300

    Fix bugs and add logging class

--- SimpleTGBot/SimpleTGBot/Logging.cs
+++ SimpleTGBot/SimpleTGBot/Logging.cs
@@ -2,6 +2,18 @@
 {
     class Logging
     {
+        public static void WriteLog(string logMessage)
+        {      
+            using (var sw = new StreamWriter("log.txt", true))
+                sw.WriteLine($"{DateTime.Now} Message: {logMessage}");
+            Console.WriteLine($"{DateTime.Now} Message: {logMessage}");
+        }
 
+        public static void ErrorWriteLog(string logMessage)
+        {
+            using (var sw = new StreamWriter("log.txt", true))
+                sw.WriteLine($"{DateTime.Now} Error: {logMessage}");
+            Console.WriteLine($"{DateTime.Now} Error: {logMessage}");
+        }
     }
 }
--- SimpleTGBot/SimpleTGBot/Program.cs
+++ SimpleTGBot/SimpleTGBot/Program.cs
@@ -4,6 +4,7 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using static System.Console;
 using static SimpleTGBot.ImageFilters;
+using static SimpleTGBot.Logging;
 
 using var cts = new CancellationTokenSource();
 var bot = new TelegramBotClient(File.ReadAllText("token.txt"), cancellationToken: cts.Token);
@@ -11,17 +12,18 @@ var me = await bot.GetMe();
 
 // Глобальные переменные
 bool flag_filters = true;   // Флаг на срабатывание /filters
-bool flag_image = true;     // Флаг на получение фото
-bool flag_width = true;     // Флаг на получение ширины изображения (нужен для фильтра "Изменение размера")
-bool flag_height = false;   // Флаг на получение высоты изображения (нужен для фильтра "Изменение размера")
-int num_filter = 0;         // Хранит номера фильтра
-int img_weight = 0;         // Хранит ширину изображения (нужен для фильтра "Изменение размера")
+int num_filter = -1;        // Хранит номер фильтра
+int img_width = 0;          // Хранит ширину изображения (нужен для фильтра "Изменение размера")
 int img_height = 0;         // Хранит высоту изображения (нужен для фильтра "Изменение размера")
-int choose_weight = 0;      // Хранит выбранную ширину изображения (нужен для фильтра "Изменение размера")
-int choose_height = 0;      // Хранит выбранную высоту изображения (нужен для фильтра "Изменение размера")
-float intensity = 1f;       // Хранит значение интенсивности (нужен для фильтров "Увеличение контраста" и "Увеличение яркости")
-byte power = 0;             // Хранит значения силы (нужен для фильтров "Усиление ... канала")
+int choose_weight = -1;     // Хранит выбранную ширину изображения (нужен для фильтра "Изменение размера")
+int choose_height = -1;     // Хранит выбранную высоту изображения (нужен для фильтра "Изменение размера")
+int user_input = -1;        // Хранит ответ пользователя
 string image_name = "";     // Хранит имя изображения
+
+/// Удаляет все (предыдущие) изображения в директории
+foreach (string extension in new string[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp" })
+    foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), $"*{extension}"))
+        File.Delete(file);
 
 bot.OnError += OnError;
 bot.OnMessage += OnMessage;
@@ -33,13 +35,14 @@ cts.Cancel(); // Остановить бота
 /// Обнуляет все глобальные переменные
 void AllGlobalVarDefault()
 {
-    flag_filters = true;      
-    flag_image = true;     
-    flag_width = true;     
-    flag_height = false;    
-    num_filter = 0;         
-    img_weight = 0;         
-    img_height = 0;
+    flag_filters = true;   
+    num_filter = -1;       
+    img_width = 0;         
+    img_height = 0;        
+    choose_weight = -1;    
+    choose_height = -1;    
+    user_input = -1;        
+    image_name = "";
 }
 
 /// Метод, который обрабатывает ошибки
@@ -51,7 +54,8 @@ async Task OnMessage(Message msg, UpdateType type)
     if (msg.Text == "/start")
     {
         AllGlobalVarDefault();
-        await bot.SendMessage(msg.Chat, $"Приветсвую {msg?.From.FirstName ?? "пользователь"}! Я вам помогу сделать базовую обработку фото. Чтобы выбрать фильтры напишите /filters"); 
+        await bot.SendMessage(msg.Chat, $"Приветсвую {msg.From.FirstName}! Я вам помогу сделать базовую обработку фото. Чтобы выбрать фильтры напишите /filters");
+        WriteLog($"Пользователь с id {msg.From.Id} написал {msg.Text}");
         return;
     }
 
@@ -71,16 +75,17 @@ async Task OnMessage(Message msg, UpdateType type)
             "7) Усиление красного канала\n" +
             "8) Усиление зелёного канала\n" +
             "9) Усиление синего канала");
+        WriteLog($"Пользователь с id {msg.From.Id} написал {msg.Text}");
         return;
     }
 
-    if (flag_filters || ((!flag_image) || num_filter == 0) && msg.Text is not { } message)
+    if (flag_filters || (image_name != "" || num_filter == -1) && msg.Text is not { } message)
     {
-        await bot.SendMessage(msg.Chat, "Я вас не понимаю.");
+        await bot.SendMessage(msg.Chat, "Я вас не понимаю");
         return;
     }
 
-    if (num_filter == 0)
+    if (num_filter == -1)
     {
         if (!new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }.Contains(msg.Text))
         {
@@ -90,15 +95,13 @@ async Task OnMessage(Message msg, UpdateType type)
         num_filter = int.Parse(msg.Text);       
     }
 
-    if (flag_image && num_filter != 0)
+    if (image_name == "" && num_filter != 0)
     {
         if(msg.Photo == null)
         {
             await bot.SendMessage(msg.Chat, "Отправьте ваше фото");
             return;
         }
-
-        flag_image = false;
 
         var fileId = msg.Photo.Last().FileId;
         var tgFile = await bot.GetFile(fileId);
@@ -110,14 +113,22 @@ async Task OnMessage(Message msg, UpdateType type)
 
         // Получаем ширину и высоту
         using (var image = SixLabors.ImageSharp.Image.Load(image_name))
-            (img_weight, img_height) = (image.Width, image.Height);
+            (img_width, img_height) = (image.Width, image.Height);
+
+        if (img_width < 10 || img_height < 10)
+        {
+            image_name = "";
+            await bot.SendMessage(msg.Chat, "Слишком маленькие размеры изображения");
+            await bot.SendMessage(msg.Chat, "Отправьте другое фото");
+            return;
+        }
     }
 
     switch (num_filter)
     {
         case 0:
             flag_filters = true;
-            num_filter = 0;
+            num_filter = -1;
 
             await bot.SendMessage(msg.Chat, "Приходите ещё :)");
             return;
@@ -127,55 +138,80 @@ async Task OnMessage(Message msg, UpdateType type)
             break;
 
         case 2:
-            if (flag_width)
+            if (choose_weight == -1)
             {
-                flag_width = false;
-                flag_height = true;
-                await bot.SendMessage(msg.Chat, $"Введите ширину (от 10 до {img_weight})");
+                choose_weight = 0;
+                await bot.SendMessage(msg.Chat, $"Введите ширину (от 10 до {img_width})");
                 return;
             }
 
-            if (msg.Text.Any(x => !char.IsDigit(x)) || int.Parse(msg.Text) < 10 || int.Parse(msg.Text) > (flag_height ? img_weight : img_height))
+            if (!int.TryParse(msg.Text, out user_input) || user_input < 10 || user_input > (choose_height == -1 ? img_width : img_height))
             {
-                await bot.SendMessage(msg.Chat, $"Нужно ввести число от 10 до {(flag_height ? img_weight : img_height)}");
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от 10 до {(choose_height == -1 ? img_width : img_height)}");
                 return;
             }
 
-            if (flag_height)
+            if (choose_height == -1)
             {
-                flag_height = false;
-                choose_weight = int.Parse(msg.Text);
+                choose_height = 0;
+                choose_weight = user_input;
                 await bot.SendMessage(msg.Chat, $"Введите высоту (от 10 до {img_height})");
                 return;
             }
 
-            choose_height = int.Parse(msg.Text);
+            choose_height = user_input;
             double aspectRatio = (double)choose_weight / choose_height;
             if (aspectRatio > 20 || aspectRatio < 0.05) 
             {
-                flag_height = true;
+                choose_height = -1;
                 await bot.SendMessage(msg.Chat, "Недопустимое соотношение сторон для отправки в телеграмм. Попробуйте другие размеры");
-                await bot.SendMessage(msg.Chat, $"Введите ширину (от 10 до {img_weight})");
+                await bot.SendMessage(msg.Chat, $"Введите ширину (от 10 до {img_width})");
                 return;
             }
-            
-            flag_width = true;
-            flag_height = false;          
 
             await ChangingSize(bot, msg.Chat, image_name, choose_weight, choose_height);
+
+            choose_weight = -1;
+            choose_height = -1;
+
             break;
 
         case 3:
             await BWFilter(bot, msg.Chat, image_name);
             break;
 
-        case 4:
-            
-            await ContrastFilter(bot, msg.Chat, image_name, 1.5f);
+        case 4:      
+            if (user_input == -1)
+            {
+                user_input = 0;
+                await bot.SendMessage(msg.Chat, $"Введите интенсивность от -10 до 10 (0 - никаких изменений)");
+                return;
+            }
+
+            if (!int.TryParse(msg.Text, out user_input) || user_input < -10 || user_input > 10)
+            {
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от -10 до 10");
+                return;
+            }
+
+            await ContrastFilter(bot, msg.Chat, image_name, user_input > 0 ? 1f + (float)user_input / 10 : ((float)user_input + 10) / 10);
             break;
 
-        case 5:
-            await BrightnessFilter(bot, msg.Chat, image_name, 1.5f);
+        case 5:             
+            if (user_input == -1)
+            {
+                user_input = 0;
+                await bot.SendMessage(msg.Chat, $"Введите интенсивность от -10 до 10 (0 - никаких изменений)");
+                return;
+            }
+
+            if (!int.TryParse(msg.Text, out user_input) || user_input < -10 || user_input > 10)
+            {
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от -10 до 10");
+                return;
+            }
+
+            await BrightnessFilter(bot, msg.Chat, image_name, user_input > 0 ? 1f + (float)user_input / 10 : ((float)user_input + 10) / 10);
             break;
 
         case 6:
@@ -183,18 +219,58 @@ async Task OnMessage(Message msg, UpdateType type)
             break;
 
         case 7:
-            await RedFilter(bot, msg.Chat, image_name, 255);
+            if (user_input == -1)
+            {
+                user_input = 0;
+                await bot.SendMessage(msg.Chat, $"Введите силу усиления от 0 до 255");
+                return;
+            }
+
+            if (!int.TryParse(msg.Text, out user_input) || user_input < 0 || user_input > 255)
+            {
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от 0 до 255");
+                return;
+            }
+
+            await RedFilter(bot, msg.Chat, image_name, (byte)user_input);
             break;
 
         case 8:
-            await GreenFilter(bot, msg.Chat, image_name, 255);
+            if (user_input == -1)
+            {
+                user_input = 0;
+                await bot.SendMessage(msg.Chat, $"Введите силу усиления от 0 до 255");
+                return;
+            }
+
+            if (!int.TryParse(msg.Text, out user_input) || user_input < 0 || user_input > 255)
+            {
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от 0 до 255");
+                return;
+            }
+
+            await GreenFilter(bot, msg.Chat, image_name, (byte)user_input);
             break;
 
         case 9:
-            await BlueFilter(bot, msg.Chat, image_name, 255);
+            if (user_input == -1)
+            {
+                user_input = 0;
+                await bot.SendMessage(msg.Chat, $"Введите силу усиления от 0 до 255");
+                return;
+            }
+
+            if (!int.TryParse(msg.Text, out user_input) || user_input < 0 || user_input > 255)
+            {
+                await bot.SendMessage(msg.Chat, $"Нужно ввести число от 0 до 255");
+                return;
+            }
+            
+            await BlueFilter(bot, msg.Chat, image_name, (byte)user_input);
             break;
     }
 
-    flag_image = true;
+    user_input = -1;
+    image_name = "";
     await bot.SendMessage(msg.Chat, "Снова отправьте фото или выберите другой фильтр /filters");
 }
